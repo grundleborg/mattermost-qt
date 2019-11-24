85e1ca936969e5f3cc58970226cf96de74daff95
George Goldberg <george@gberg.me>
2019-11-24 09:31:02 +0000

    MMVP

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QtWebEngineWidgets/QWebEngineView>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->view = new QWebEngineView(this->ui->tab);
+    this->ui->hLayout->addWidget(this->view);
+    view->load(QUrl("https://community.mattermost.com/"));
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+class QWebEngineView;
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -17,5 +19,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QWebEngineView *view;
 };
 #endif // MAINWINDOW_H
